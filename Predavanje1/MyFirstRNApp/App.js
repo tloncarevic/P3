2c24813fd2dfe82d552eef8d24a0a745e6a8d97e
tloncarevic <72250321+tloncarevic@users.noreply.github.com>
2021-10-04 09:19:30 +0200

    Update teksta

--- Predavanje1/MyFirstRNApp/App.js
+++ Predavanje1/MyFirstRNApp/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Dobro jutro radni narode</Text>
+      <Text>Jutarnje programiranje</Text>
       <StatusBar style="auto" />
     </View>
   );
